fba963445a1671d1da6acada179a8bdb30e698e0
dbsasser <danielbsasser@gmail.com>
2020-05-29 13:29:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 // Your code here
+function mapToNegativize(ray) {
+    let result = ray.map(function(e) {
+        return e * -1
+    });
+
+    return result
+}
+
+function mapToNoChange(ray) {
+    let result = ray.map(function(e) {
+        return e
+    });
+
+    return result
+}
+
+function mapToDouble(ray) {
+    let result = ray.map(function(e) {
+        return e * 2
+    });
+
+    return result
+}
+
+function mapToSquare(ray) {
+    let result = ray.map(function(e) {
+        return e ** 2
+    });
+
+    return result
+}
+
+function reduceToTotal (ray, start=0) {
+    let total = start 
+
+    for (let i = 0; i < ray.length; i++) {
+        total += ray[i]
+    }
+
+    return total
+}
+
+function reduceToAllTrue(ray) {
+    for (let i = 0; i < ray.length; i++){
+        if (!ray[i]) {
+            return false
+        }
+    }
+
+    return true
+}
+
+function reduceToAnyTrue(ray) {
+    for (let i = 0; i < ray.length; i++){
+        if (ray[i]) {
+            return true
+        }
+    }
+    return false
+    
+}
